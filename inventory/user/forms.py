57724dedcbc53fe7860ca12e8fb8f02b0abb9d35
Tooni <prashanth04@gmail.com>
2022-08-02 23:38:24 +0530

    Login and Logout Process

--- inventory/user/forms.py
+++ inventory/user/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-
 class CreateUSerForm(UserCreationForm):
     email = forms.EmailField()
 
