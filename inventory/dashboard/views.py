38cf858bff160a5138369dd6ecdec4f9ae49541b
Tooni <prashanth04@gmail.com>
2022-08-03 08:02:54 +0530

    user_level_permissions

--- inventory/dashboard/views.py
+++ inventory/dashboard/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 # Create your views here.
 
 
+@login_required
 def home(request):
     return render(request, 'dashboard/home.html')
 
 
+@login_required
 def staff(request):
     return render(request, 'dashboard/staff.html')
 
-
+@login_required()
 def product(request):
     return render(request, 'dashboard/product.html')
 
+@login_required()
 def orders(request):
     return render(request, 'dashboard/orders.html')
